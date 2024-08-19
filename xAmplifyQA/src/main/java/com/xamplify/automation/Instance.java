fc2feff333ee378a34649f13a5a24e419f2091fb
agayatri <agayatri@stratapps.com>
2024-08-19 16:53:53 +0530

    commit for exisitng code

---
+++ xAmplifyQA/src/main/java/com/xamplify/automation/Instance.java
@@ -0,0 +1,26 @@
+package com.xamplify.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Instance {
+
+	public static WebDriver driver;
+
+	  public static WebDriver getInstance() 	  {
+		  if (driver == null) 
+		   {
+			  
+			WebDriverManager.chromedriver().setup();  
+		   	ChromeOptions options = new ChromeOptions();
+		   	options.addArguments("incognito");
+		   	options.addArguments("use-fake-ui-for-media-stream"); 
+		   	driver = new ChromeDriver(options);
+		   }
+		   return driver;
+		 }
+}
+
