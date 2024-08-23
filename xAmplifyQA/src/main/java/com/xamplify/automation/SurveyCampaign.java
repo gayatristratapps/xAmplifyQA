3ad295ba32cc9e2fd2d0221445229630da81169d
chmounika <chmounika@chmounika-sa.stratapps.com>
2024-08-23 17:24:38 +0530

    Committed- Survey Through campaign (Launch, Schedule, Save)

--- xAmplifyQA/src/main/java/com/xamplify/automation/SurveyCampaign.java
+++ xAmplifyQA/src/main/java/com/xamplify/automation/SurveyCampaign.java
@@ -24,7 +24,7 @@ public class SurveyCampaign {
 	WebDriver driver = Instance.getInstance();
 
 	Properties properties = PropertiesFile
-			.readPropertyFile("D:\\git\\xAmplify-Automation\\src\\main\\resources\\Campaign.properties");
+			.readPropertyFile("D:\\git\\xAmplifyQA\\xAmplifyQA\\src\\main\\resources\\Campaign.properties");
 
 	final Logger logger = LogManager.getLogger(SurveyCampaign.class);
 
