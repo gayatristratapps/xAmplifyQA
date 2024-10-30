5b920ecb22fb1f6bc464a20b86d4a45601e83ab8
agayatri <agayatri@agayatri-sa.stratapps.com>
2024-10-30 18:30:47 +0530

    Commit for One_at_a_time in contacts module

--- xAmplifyQA/src/main/java/com/xamplify/automation/Contacts.java
+++ xAmplifyQA/src/main/java/com/xamplify/automation/Contacts.java
@@ -1,5 +1,6 @@
 package com.xamplify.automation;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Random;
@@ -11,9 +12,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.xamplifycon.util.XamplifyUtil_contacts;
 
 public class Contacts {
 
@@ -24,7 +28,9 @@ public class Contacts {
 
 	@Test(priority = 1, enabled = true)
 
-	public void con_oneatatime() throws InterruptedException, SQLException {
+	public void contacts_hover() throws InterruptedException, SQLException {
+
+		logger.debug("Creating contact using one ata time");
 
 		Thread.sleep(5000);
 
@@ -41,6 +47,12 @@ public class Contacts {
 		WebElement contacts = driver.findElement(By.xpath(properties.getProperty("hovercontacts")));
 		actions.moveToElement(contacts).build().perform();
 
+	}
+
+	@Test(priority = 2, enabled = true)
+
+	public void con_oneatatime() throws InterruptedException, SQLException {
+
 		WebDriverWait wait_acon = new WebDriverWait(driver, 60);
 
 		// Wait till the element is not visible
@@ -56,10 +68,10 @@ public class Contacts {
 
 		Random randomGenerator = new Random();
 		int randomInt = randomGenerator.nextInt(1000);
-		emailText.sendKeys("gayatri" + randomInt + "@gmail.com");
+		emailText.sendKeys("gayatri" + randomInt + "@gmail.com"); // will generate automatic number to append
 		Thread.sleep(1000);
 
-		driver.findElement(By.xpath(properties.getProperty("con_legalbasis"))).click();
+		driver.findElement(By.xpath(properties.getProperty("con_legalbasis"))).click(); // click for legal basis
 		Thread.sleep(2000);
 
 		WebElement con_legal = driver.findElement(By.xpath(properties.getProperty("con_legalbasis")));
@@ -70,20 +82,24 @@ public class Contacts {
 
 		Thread.sleep(2000);
 
-		driver.findElement(By.xpath(properties.getProperty("con_firstname"))).sendKeys("gayatri");
+		driver.findElement(By.xpath(properties.getProperty("con_firstname"))).sendKeys("GAYATRI");  //send firstname data
 
+		driver.findElement(By.xpath(properties.getProperty("con_lastname"))).sendKeys("A");  //send lastname data
+		Thread.sleep(3000);
+
+		WebElement drp = driver.findElement(By.xpath(properties.getProperty("con_comp_dropdown"))); 
+		Actions actions2 = new Actions(driver);
+		actions2.moveToElement(drp).click().sendKeys("xAmplify").perform();
+		Thread.sleep(4000);
+
+		driver.findElement(By.id("title")).sendKeys("sse");   //send data for title
+		driver.findElement(By.id("address")).sendKeys("sri maartuhi homes, citizens colony, lingampally");
+		driver.findElement(By.id("city")).sendKeys("Hyderabad");  //send data for city
+		driver.findElement(By.id("state")).sendKeys("Telegana");  //send data for state
+		driver.findElement(By.id("zip")).sendKeys("5000S0");  //send data for zipcode
+		Thread.sleep(1000);
+		driver.findElement(By.xpath(properties.getProperty("con_mobileno"))).sendKeys("9490925098"); // enter for phone number
 		Thread.sleep(2000);
-
-		driver.findElement(By.xpath(properties.getProperty("con_lastname"))).sendKeys("A");
-
-		driver.findElement(By.xpath(properties.getProperty("con_mobileno"))).sendKeys("9490925098"); // enter for
-																										// phoneno
-		Thread.sleep(2000);
-
-		driver.findElement(By.xpath(properties.getProperty("con_title"))).sendKeys("sse");
-
-		Thread.sleep(2000);
-
 		driver.findElement(By.xpath(properties.getProperty("con_addbutton"))).click(); // click for add button
 		Thread.sleep(3000);
 
@@ -100,20 +116,39 @@ public class Contacts {
 
 			WebElement errmsg = driver.findElement(By.xpath(properties.getProperty("con_existname")));
 			String Actual_res = errmsg.getText();
-			String excepted_res = "Contact List name already exists";
-			Assert.assertEquals(excepted_res, Actual_res);
+			String excepted_res = "Contact List name already exists";   
+			Assert.assertEquals(excepted_res, Actual_res);                    //check for validation for exisitng list
 			driver.findElement(By.xpath(properties.getProperty("con_oat_listfield")))
 					.sendKeys("_A1" + "_" + System.currentTimeMillis());
 			Thread.sleep(2000);
-			driver.findElement(By.xpath(properties.getProperty("con_oat_Save"))).click();
+			driver.findElement(By.xpath(properties.getProperty("con_oat_Save"))).click();  //click for save 
 			Thread.sleep(2000);
 
-		} catch (Exception e) {
-			System.out.println("contact list name already exists");
+		} catch (Exception e1) {
+			System.out.println("exception occurred in catch block");
 
 		}
 
 		driver.findElement(By.xpath(properties.getProperty("con_accept"))).click(); // click for accept button
 		Thread.sleep(2000);
 	}
+
+	@Test(priority = 2, enabled = false)
+
+	public void con_uploadcsv() throws InterruptedException, SQLException, IOException {
+
+		logger.debug("Starting creating contact using upload a csv");
+		contacts_hover();
+		WebDriverWait wait_acon = new WebDriverWait(driver, 60);
+
+		// Wait till the element is not visible
+		WebElement acon1 = wait_acon
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(properties.getProperty("Addcontacts"))));
+		acon1.click();
+		driver.findElement(By.id("uploadCSV")).click(); // click on the upload csv
+		Thread.sleep(5000);
+		XamplifyUtil_contacts.executeRuntimeProcess();
+
+	}
+
 }
