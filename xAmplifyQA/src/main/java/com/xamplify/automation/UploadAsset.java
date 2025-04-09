0b9cd7a0b2fd20b521dbfd0959381bb5f3ad7c4d
chmounika <chmounika@chmounika-1sa.stratapps.com>
2025-04-09 18:34:48 +0530

    DAM_changes

--- xAmplifyQA/src/main/java/com/xamplify/automation/UploadAsset.java
+++ xAmplifyQA/src/main/java/com/xamplify/automation/UploadAsset.java
@@ -37,16 +37,33 @@ public class UploadAsset {
 		Thread.sleep(3000);
 
 	}
+	
+	
+	public void publish_to_partner() throws InterruptedException {
+		
+		XamplifyUtil.sendTextEvent(properties.getProperty("clickon_selectpartner_search"), "PartnerAuto");
+		Thread.sleep(2000);
+		XamplifyUtil.callClickEvent(properties.getProperty("clickon_selectpartner_search_icon"));
+		Thread.sleep(5000);
+		XamplifyUtil.callClickEvent(properties.getProperty("clickon_selectedpartner_rightarrow"));
+		Thread.sleep(2000);
+		XamplifyUtil.callClickEvent(properties.getProperty("clickon_entity_info_checkbox"));
+		Thread.sleep(2000);
+
+		XamplifyUtil.callClickEvent(properties.getProperty("clickon_saveandpublish"));
+		Thread.sleep(5000);
+
+		
+	}
 
 // UPLOAD PNG FILE
 
 	@Test(priority = 0, enabled = true)
 	public void uploadasset_png() throws InterruptedException, IOException, AWTException {
-
-		WebElement browse = driver.findElement(By.xpath(properties.getProperty("BrowseButton")));
+		
+		WebElement browse = driver.findElement(By.xpath(properties.getProperty("clicktouploadbutton")));
 		browse.click(); // click on Browse
 		Thread.sleep(3000);
-
 		Robot robot = new Robot(); // use robot class to upload file -- create object
 
 // store the path of the file to be uploaded using stringselection class object
@@ -123,7 +140,8 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
+		
 		Thread.sleep(2000);
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
@@ -140,13 +158,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 //	UPLOAD CSV FILE
 
-	@Test(priority = 1, enabled = true)
+	@Test(priority = 1, enabled = false)
 	public void uploadasset_csv() throws InterruptedException, IOException, AWTException {
 		hoverandclickoncontent_module();
 		WebElement browse = driver.findElement(By.xpath(properties.getProperty("BrowseButton"))); // click on Browse
@@ -254,13 +271,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(5000);
 		XamplifyUtil.clickElementWithWait(driver, properties.getProperty("Refresh"), 30); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 //UPLOAD ASSET THROUGH BOX ACCOUNT
 
-	@Test(priority = 2, enabled = true)
+	@Test(priority = 2, enabled = false)
 	public void videoasset() throws InterruptedException, IOException, AWTException {
 		hoverandclickoncontent_module();
 		Thread.sleep(5000);
@@ -338,22 +354,12 @@ public class UploadAsset {
 				Thread.sleep(2000);
 				driver.switchTo().defaultContent();
 
-				XamplifyUtil.sendTextEvent(properties.getProperty("clickon_selectpartner_search"), "PartnerAuto");
+				publish_to_partner();//publish to a partner
 				Thread.sleep(2000);
-				XamplifyUtil.callClickEvent(properties.getProperty("clickon_selectpartner_search_icon"));
-				Thread.sleep(5000);
-				XamplifyUtil.callClickEvent(properties.getProperty("clickon_selectedpartner_rightarrow"));
-				Thread.sleep(2000);
-				XamplifyUtil.callClickEvent(properties.getProperty("clickon_entity_info_checkbox"));
-				Thread.sleep(2000);
-
-				XamplifyUtil.callClickEvent(properties.getProperty("clickon_saveandpublish"));
-				Thread.sleep(5000);
 
 				logger.info("asset(jpeg) has been uploaded from box account");
 
 				XamplifyUtil.clickElementWithWait(driver, properties.getProperty("Refresh"), 50);
-				logger.info("Refresh icon has been clicked");
 				logger.info("asset has been published successfully through box account");
 
 			}
@@ -491,7 +497,7 @@ public class UploadAsset {
 
 	// UPLOAD PPT FILE
 
-	@Test(priority = 4, enabled = true)
+	@Test(priority = 4, enabled = false)
 	public void uploadasset_ppt() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -576,8 +582,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -593,13 +600,13 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
+		logger.info("PPT asset uploaded successfully");
 		Thread.sleep(4000);
 	}
 
 	// UPLOAD PPTX FILE
 
-	@Test(priority = 5, enabled = true)
+	@Test(priority = 5, enabled = false)
 	public void uploadasset_pptx() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -684,8 +691,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -701,13 +709,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 // UPLOAD DOC FILE
 
-	@Test(priority = 6, enabled = true)
+	@Test(priority = 6, enabled = false)
 	public void uploadasset_doc() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -793,8 +800,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -810,13 +818,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 	// UPLOAD DOCX FILE
 
-	@Test(priority = 7, enabled = true)
+	@Test(priority = 7, enabled = false)
 	public void uploadasset_docx() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -902,8 +909,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -919,13 +927,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 // UPLOAD PDF FILE
 
-	@Test(priority = 8, enabled = true)
+	@Test(priority = 8, enabled = false)
 	public void uploadasset_pdf() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -1011,8 +1018,11 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
+//		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+//		Thread.sleep(2000);
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -1028,13 +1038,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 	// UPLOAD JPG FILE
 
-	@Test(priority = 9, enabled = true)
+	@Test(priority = 9, enabled = false)
 	public void uploadasset_jpg() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -1120,8 +1129,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -1138,13 +1148,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 // UPLOAD GIF FILE
 
-	@Test(priority = 10, enabled = true)
+	@Test(priority = 10, enabled = false)
 	public void uploadasset_gif() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -1230,8 +1239,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -1248,13 +1258,12 @@ public class UploadAsset {
 		}
 		Thread.sleep(3000);
 		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
-		logger.info("Refresh icon has been clicked");
 		Thread.sleep(4000);
 	}
 
 	// UPLOAD HTML FILE
 
-	@Test(priority = 11, enabled = true)
+	@Test(priority = 11, enabled = false)
 	public void uploadasset_html() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -1340,8 +1349,9 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -1364,7 +1374,7 @@ public class UploadAsset {
 
 	// UPLOAD TXT FILE
 
-	@Test(priority = 12, enabled = true)
+	@Test(priority = 12, enabled = false)
 	public void uploadasset_txt() throws InterruptedException, IOException, AWTException {
 
 		hoverandclickoncontent_module();
@@ -1450,8 +1460,120 @@ public class UploadAsset {
 		// enter description
 		Thread.sleep(2000);
 		driver.switchTo().defaultContent();
-		XamplifyUtil.callClickEvent(properties.getProperty("Submit")); // click on submit
+		publish_to_partner();//publish to a partner
 		Thread.sleep(2000);
+
+		logger.info("clicked on submit button after filling all mandaoty fields");
+
+		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
+				.getText();
+		Thread.sleep(3000);
+		String expectedresult_asset = "Uploaded Successfully";
+		if (expectedresult_asset.equals(actualresult_asset)) {
+
+			System.out.println("Asset file(TXT)uploaded successfully");
+			logger.info("Asset file(TXT)uploaded successfully");
+		} else {
+			System.out.println("Asset file(TXT) not uploaded");
+			logger.info("Asset file(TXT) is not uploaded");
+		}
+		Thread.sleep(3000);
+		XamplifyUtil.callClickEvent(properties.getProperty("Refresh")); // click on refresh icon
+
+		Thread.sleep(4000);
+	}
+
+	
+	
+	@Test(priority = 13, enabled = false)
+	public void uploadasset_zip() throws InterruptedException, IOException, AWTException {
+
+		hoverandclickoncontent_module();
+		Thread.sleep(3000);
+
+		WebElement browse = driver.findElement(By.xpath(properties.getProperty("BrowseButton")));
+		browse.click(); // click on Browse
+		Thread.sleep(3000);
+
+		Robot robot = new Robot(); // use robot class to upload file -- create object
+
+		// store the path of the file to be uploaded using stringselection class object
+		StringSelection filepath = new StringSelection("D:\\git\\xAmplifyQA\\xAmplifyQA\\files\\zip_file.zip");
+		// create a string selection object copy above path to clipboard
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(filepath, null);
+		// here path is avaliable in clipboard
+		Thread.sleep(4000);
+		robot.keyPress(KeyEvent.VK_CONTROL);// press ctrl
+		robot.keyPress(KeyEvent.VK_V); // press V
+		robot.keyRelease(KeyEvent.VK_V); // release V
+		robot.keyRelease(KeyEvent.VK_CONTROL);// relase ctrl
+		robot.keyPress(KeyEvent.VK_ENTER);// press enter
+		robot.keyRelease(KeyEvent.VK_ENTER); // release enter
+//										logger.info("png file uploaded using robot class");
+		Thread.sleep(3000);
+		XamplifyUtil.callClickEvent(properties.getProperty("Thumbnail")); // click on thumbnail icon
+		Thread.sleep(2000);
+		StringSelection filepath1 = new StringSelection("D:\\git\\xAmplifyQA\\xAmplifyQA\\files\\Thumbnail_file.jpg");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(filepath1, null); // here path is avaliable in
+		// create a string selection object copy above path to clipboard // clipboard
+
+		Thread.sleep(2000);
+		robot.keyPress(KeyEvent.VK_CONTROL);// press ctrl
+		robot.keyPress(KeyEvent.VK_V); // press V
+		robot.keyRelease(KeyEvent.VK_V); // release V
+		robot.keyRelease(KeyEvent.VK_CONTROL);// relase ctrl
+		robot.keyPress(KeyEvent.VK_ENTER);// press enter
+		robot.keyRelease(KeyEvent.VK_ENTER); // release enter
+		Thread.sleep(2000);
+
+		XamplifyUtil.callClickEvent(properties.getProperty("Savechanges")); // click on save changes
+		Thread.sleep(2000);
+		logger.info("thumbnail uploaded using robot class");
+
+		XamplifyUtil.sendTextWithTimestamp(properties.getProperty("Name"), "ZIP_name", "asset");
+		Thread.sleep(2000);
+		XamplifyUtil.callClickEvent(properties.getProperty("folder_dropdown_click1")); // click on folder dropdown
+		Thread.sleep(2000);
+		WebElement fold_search = driver.findElement(By.xpath(properties.getProperty("folder_Search")));
+		fold_search.sendKeys("xamplify2024-Default-Folder");
+		fold_search.click();
+		Thread.sleep(1000);
+		XamplifyUtil.callClickEvent(properties.getProperty("folder_selection1")); // click on folder dropdown
+		Thread.sleep(3000);
+		XamplifyUtil.clickElementWithWait(driver, properties.getProperty("tag_plusicon"), 50);
+		// XamplifyUtil.callClickEvent(properties.getProperty("tag_plusicon"));
+		Thread.sleep(1000);
+		XamplifyUtil.callClickEvent(properties.getProperty("add_tagbutton"));
+		Thread.sleep(2000);
+		XamplifyUtil.callClickEvent(properties.getProperty("tag_text_click"));
+		WebElement enter = driver.findElement(By.xpath(properties.getProperty("tag_text_click")));
+		enter.sendKeys(properties.getProperty("ZIP_tag") + "_" + System.currentTimeMillis());
+		enter.sendKeys(Keys.ENTER);
+		Thread.sleep(1000);
+		XamplifyUtil.callClickEvent(properties.getProperty("tag_savebutton"));
+		Thread.sleep(2000);
+		XamplifyUtil.callClickEvent(properties.getProperty("tag_select"));
+		Thread.sleep(2000);
+		XamplifyUtil.callClickEvent(properties.getProperty("tag_savebutton"));
+		Thread.sleep(1000);
+
+		XamplifyUtil.clickElementWithWait(driver, (properties.getProperty("clickon_add_moretags")), 30);
+		Thread.sleep(1000);
+		XamplifyUtil.sendTextEvent(properties.getProperty("clickon_search_under_add_moretags"), "txt");
+		Thread.sleep(1000);
+		XamplifyUtil.excelDownload();
+		XamplifyUtil.callClickEvent(properties.getProperty("select_searched_tag"));
+		Thread.sleep(1000);
+		XamplifyUtil.callClickEvent(properties.getProperty("clickon_update_under_add_motetags"));
+		Thread.sleep(1000);
+		driver.switchTo().frame(0);
+		XamplifyUtil.sendTextEvent(properties.getProperty("Descriptionclick"), ("Asset_Description"));
+		// enter description
+		Thread.sleep(2000);
+		driver.switchTo().defaultContent();
+		publish_to_partner();//publish to a partner
+		Thread.sleep(2000);
+
 		logger.info("clicked on submit button after filling all mandaoty fields");
 
 		String actualresult_asset = driver.findElement(By.xpath(properties.getProperty("asset_Successmessage")))
@@ -1473,3 +1595,5 @@ public class UploadAsset {
 	}
 
 }
+
+
